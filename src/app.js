602097d3f786728f0a383eaa7137e06f04248aa5
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-02-15 15:45:49 -0800

    use cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 	res.contentType("application/json; charset=utf-8");
 	next();
 });
-app.use("*", cors());
+app.use(cors("*"));
 
 // Route handlers
 app.use("/posts", PostsRouter);
