c55002f80e8f2c4a8b495c3c0752b3aa68f9e7a3
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-02-15 19:21:43 -0800

    pure cors

--- src/middleware/app.js
+++ src/middleware/app.js
@@ -8,17 +8,5 @@ export const setCommonMiddleware = (app) => {
 		res.contentType("application/json; charset=utf-8");
 		next();
 	});
-	app.use((req, res, next) => {
-		res.header("Access-Control-Allow-Credentials", "true");
-		res.header(
-			"Access-Control-Allow-Methods",
-			"GET,PUT,POST,DELETE,OPTIONS",
-		);
-		res.header(
-			"Access-Control-Allow-Headers",
-			"Origin,X-Requested-With,X-HTTP-Method-Override,Content-Type,Accept,content-type,application/json",
-		);
-		next();
-	});
 	app.use(cors());
 };
